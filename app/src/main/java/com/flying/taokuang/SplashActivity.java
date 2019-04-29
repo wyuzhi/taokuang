509e31600326082527287791151ef28c3a321079
huangguopu <huangguopu@bytedance.com>
2019-04-29 16:47:36 +0800

    修改启动页

--- app/src/main/java/com/flying/taokuang/SplashActivity.java
+++ app/src/main/java/com/flying/taokuang/SplashActivity.java
@@ -2,25 +2,18 @@ package com.flying.taokuang;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import com.flying.baselib.utils.app.MainThread;
 
 public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-
-//        SYSTEM_UI_FLAG_FULLSCREEN表示全屏的意思，也就是会将状态栏隐藏
-        //设置系统UI元素的可见性
-
-
-        new Handler().postDelayed(new Runnable() {
+        MainThread.postDelayed(new Runnable() {
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 SplashActivity.this.finish();//结束本Activity
             }
